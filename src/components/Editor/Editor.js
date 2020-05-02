884800083f640645dc5746a35ebe7ca8012af126
Cameron Chardukian <camchardukian@gmail.com>
2020-05-02 19:16:39 +0700

    fix/context-implementation

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,13 +2,13 @@ import React, { useContext } from 'react'
 import { MarkdownContext } from '../../providers/MarkdownProvider';
 
 const Editor = () => {
-  console.log('hoooo', MarkdownContext)
-  // const { editorContent, handleSetEditorContent } = useContext(MarkdownContext);
+  // console.log('hoooo', MarkdownContext)
+  const { editorContent, handleSetEditorContent } = useContext(MarkdownContext);
   return (
     <div>
       <h1>I am the Editor</h1>
-      {/* <textarea id="editor" value={editorContent} onChange={(event) => handleSetEditorContent(event.target.value)}>
-      </textarea> */}
+      <textarea id="editor" value={editorContent} onChange={(event) => handleSetEditorContent(event.target.value)}>
+      </textarea>
     </div>
   )
 }
