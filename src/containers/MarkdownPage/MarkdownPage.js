cb06e086f825effe80df963b7bcc8ab066d8a3fd
Cameron Chardukian <camchardukian@gmail.com>
2020-05-02 19:40:20 +0700

    feature/preview

--- src/containers/MarkdownPage/MarkdownPage.js
+++ src/containers/MarkdownPage/MarkdownPage.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Editor from '../../components/Editor'
+import React, { useContext } from 'react';
+import Editor from '../../components/Editor';
+import { MarkdownContext } from '../../providers/MarkdownProvider';
+import './styles.scss';
 
 const MarkdownPage = () => {
+  const { editorContent } = useContext(MarkdownContext);
   return (
     <div>
       <h1>I am the Markdown Page</h1>
       <Editor />
       <div id="preview">
+        <p>{editorContent}</p>
       </div>
     </div>
   )
